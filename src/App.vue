f77655b8c35ab19bec5cd61345c68dc7c2f631e0
Eider Agudelo <eider841agudelo@hotmail.com>
2024-05-23 21:52:16 -0400

    Se crea la extructura y se consume los datos de la API

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,66 @@ import axios from "axios"
 export default {
   data() {
     return {
-      personaMale: [],
-      personaFemale: null,
+      users: [],
       chat1: "",
       chat2: ""
     }
   },
 
-  async mounted() {
-    try {
-      const url = "https://randomuser.me/api/?gender="
+  methods: {
+    async usersDataAPI(user) {
+      try {
+        const url = "https://randomuser.me/api/?gender="
 
-      const { data } = await axios.get(`${url}male`)
-      //const {female} = await axios.get(`${url}female`)
+        const { data } = await axios.get(`${url}${user}`)
+        return data.results
 
-      this.personaMale = data 
-      console.log(this.personaMale);
-      //this.personaFemale = female
-    } catch (error) {
-      console.error("Error fetching data:", error)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      }
+
+      console.log(this.users);
     }
+  },
+
+  async mounted() {
+    const maleUser = await this.usersDataAPI("male")
+    const femaleUser = await this.usersDataAPI("female")
+
+    this.users = [...maleUser, ...femaleUser]
   }
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{ personaMale?.name?.first }} {{ personaMale?.name?.last }}</h1>
+  <div class="container">
+
+    <div class="row mt-5">
+      
+      <div class="col-sm-3">
+        <div v-for="(user, i) in users" :key="i">
+          <div v-if="i == 0">
+            <h1>{{ user.name?.first }}</h1>
+            <img :src="user.picture?.large" alt="" srcset="">
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quos similique magnam enim amet ratione,
+          earum a veritatis voluptatum facere error illum ex vitae sequi totam deserunt eligendi et maiores?</h1>
+      </div>
+
+      <div class="col-sm-3">
+        <div v-for="(user, i) in users" :key="i">
+          <div v-if="i == 1">
+            <h1>{{ user.name?.first }}</h1>
+            <img :src="user.picture?.large" alt="" srcset="">
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
